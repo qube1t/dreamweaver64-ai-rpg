63bc21ad84ac437431ffb542227ad540443e3dd9
Samuel Kurian <samuelrkurian@gmail.com>
2023-09-05 00:02:35 +1200

    added action field to change the action of the character using y-offset of the sprite sheet

--- src/main/java/nz/ac/auckland/se206/components/Character.java
+++ src/main/java/nz/ac/auckland/se206/components/Character.java
@@ -1,7 +1,6 @@
 package nz.ac.auckland.se206.components;
 
 import java.io.IOException;
-
 import javafx.animation.Animation;
 import javafx.beans.NamedArg;
 import javafx.fxml.FXML;
@@ -13,137 +12,165 @@ import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 import nz.ac.auckland.se206.SpriteAnimation;
 
-public class Character extends AnchorPane{
-    @FXML
-    private ImageView active_img;
+public class Character extends AnchorPane {
+  @FXML private ImageView active_img;
 
-    private int columns;  //=   4;
-    private int count;    //=  11;
-    private int offset_x; //=  18;
-    private int offset_y; //=  25;
-    private int frame_width;    //= 374;
-    private int frame_height;   //= 243;
+  private int columns; // =   4;
+  private int count; // =  11;
+  private int offset_x; // =  18;
+  private int offset_y; // =  25;
+  private int frame_width; // = 374;
+  private int frame_height; // = 243;
 
-    private String spriteSheet;
+  private String spriteSheet;
 
-    private SpriteAnimation animation;
+  private SpriteAnimation animation;
 
-    private boolean animating =false;
+  private boolean animating = false;
 
-    
-    public String getSpriteSheet() {
-        return spriteSheet;
+  public boolean isAnimating() {
+    return animating;
+  }
+
+  private int action = 0;
+
+  public int getAction() {
+    return action;
+  }
+
+  public void setAction(int action) {
+    this.action = action;
+    // this.offset_y = offset_y + 64 * action;
+
+    if (animating != true) initElements();
+  }
+
+  public String getSpriteSheet() {
+    return spriteSheet;
+  }
+
+  public Character(
+      @NamedArg("spriteSheet") String spriteSheet,
+      @NamedArg("columns") int columns,
+      @NamedArg("count") int count,
+      @NamedArg("offset_x") int offset_x,
+      @NamedArg("offset_y") int offset_y,
+      @NamedArg("frame_width") int frame_width,
+      @NamedArg("frame_height") int frame_height) {
+
+    this.spriteSheet = spriteSheet;
+    this.columns = columns;
+    this.count = count;
+    this.offset_x = offset_x;
+    this.offset_y = offset_y;
+    this.frame_width = frame_width;
+    this.frame_height = frame_height;
+
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/character.fxml"));
+    fxmlLoader.setRoot(this);
+    fxmlLoader.setController(this);
+
+    try {
+      fxmlLoader.load();
+    } catch (IOException exception) {
+      throw new RuntimeException(exception);
     }
 
-    public Character(@NamedArg("spriteSheet") String spriteSheet, @NamedArg("columns") int columns, @NamedArg("count") int count, @NamedArg("offset_x") int offset_x, @NamedArg("offset_y") int offset_y, @NamedArg("frame_width") int frame_width, @NamedArg("frame_height") int frame_height){
+    System.out.println(spriteSheet);
+    initElements();
+  }
 
-        this.spriteSheet = spriteSheet;
-        this.columns = columns;
-        this.count = count;
-        this.offset_x = offset_x;
-        this.offset_y = offset_y;
-        this.frame_width = frame_width;
-        this.frame_height = frame_height;
+  private void initElements() {
+    Image sprite_sheet_img = new Image(getClass().getResource(spriteSheet).toExternalForm());
+    active_img.setImage(sprite_sheet_img);
+    active_img.setViewport(
+        new Rectangle2D(offset_x, offset_y + 64 * action, frame_width, frame_height));
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/character.fxml"));
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
+    animation =
+        new SpriteAnimation(
+            active_img,
+            Duration.millis(1000),
+            count,
+            columns,
+            offset_x,
+            offset_y + 64 * action,
+            frame_width,
+            frame_height);
+  }
 
-        try {
-            fxmlLoader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-        
-        System.out.println(spriteSheet);
-        initElements();
+  public void startAnimation() {
+    if (animating != true) {
+      animation.setCycleCount(Animation.INDEFINITE);
+      animation.play();
+      animating = true;
     }
+  }
 
-    private void initElements() {
-        Image sprite_sheet_img = new Image(getClass().getResource(spriteSheet).toExternalForm());
-        active_img.setImage(sprite_sheet_img);
-        active_img.setViewport(new Rectangle2D(offset_x, offset_y, frame_width, frame_height));
-        
-        animation = new SpriteAnimation(
-                active_img,
-                Duration.millis(1000),
-                count, columns,
-                offset_x, offset_y,
-                frame_width, frame_height
-        );
-    }
+  public void endAnimation() {
+    animation.stop();
+    active_img.setViewport(
+        new Rectangle2D(offset_x, offset_y + 64 * action, frame_width, frame_height));
+    animating = false;
+  }
 
-    public void startAnimation(){
-        if (animating != true){
-        animation.setCycleCount(Animation.INDEFINITE);
-        animation.play();
-    }
-    }
+  public void setSpriteSheet(String sprite_sheet) {
+    this.spriteSheet = sprite_sheet;
+    // initElements();
+  }
 
-    public void endAnimation(){
-        animation.stop();
-        active_img.setViewport(new Rectangle2D(offset_x, offset_y, frame_width, frame_height));
-        animating = false;
-    }
+  // public ImageView getActive_img() {
+  //     return active_img;
+  // }
 
-    public void setSpriteSheet(String sprite_sheet) {
-        this.spriteSheet = sprite_sheet;
-        // initElements();
-    }
+  // public void setActive_img(ImageView active_img) {
+  //     this.active_img = active_img;
+  // }
 
-    // public ImageView getActive_img() {
-    //     return active_img;
-    // }
-    
-    // public void setActive_img(ImageView active_img) {
-    //     this.active_img = active_img;
-    // }
-    
-    public int getColumns() {
-        return columns;
-    }
+  public int getColumns() {
+    return columns;
+  }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
+  public void setColumns(int columns) {
+    this.columns = columns;
+  }
 
-    public int getCount() {
-        return count;
-    }
+  public int getCount() {
+    return count;
+  }
 
-    public void setCount(int count) {
-        this.count = count;
-    }
+  public void setCount(int count) {
+    this.count = count;
+  }
 
-    public int getOffset_x() {
-        return offset_x;
-    }
+  public int getOffset_x() {
+    return offset_x;
+  }
 
-    public void setOffset_x(int offset_x) {
-        this.offset_x = offset_x;
-    }
+  public void setOffset_x(int offset_x) {
+    this.offset_x = offset_x;
+  }
 
-    public int getOffset_y() {
-        return offset_y;
-    }
+  public int getOffset_y() {
+    return offset_y;
+  }
 
-    public void setOffset_y(int offset_y) {
-        this.offset_y = offset_y;
-    }
+  public void setOffset_y(int offset_y) {
+    this.offset_y = offset_y;
+  }
 
-    public int getFrame_width() {
-        return frame_width;
-    }
+  public int getFrame_width() {
+    return frame_width;
+  }
 
-    public void setFrame_width(int frame_width) {
-        this.frame_width = frame_width;
-    }
+  public void setFrame_width(int frame_width) {
+    this.frame_width = frame_width;
+  }
 
-    public int getFrame_height() {
-        return frame_height;
-    }
+  public int getFrame_height() {
+    return frame_height;
+  }
 
-    public void setFrame_height(int frame_height) {
-        this.frame_height = frame_height;
-    }
+  public void setFrame_height(int frame_height) {
+    this.frame_height = frame_height;
+  }
 }
