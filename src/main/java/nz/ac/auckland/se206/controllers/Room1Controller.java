457f60b525f9009934ac6942d05f5446af3450fc
Samuel Kurian <samuelrkurian@gmail.com>
2023-09-11 02:45:33 +1200

    added obstacle rectangles

--- src/main/java/nz/ac/auckland/se206/controllers/Room1Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room1Controller.java
@@ -2,6 +2,7 @@ package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.components.Character;
@@ -15,13 +16,29 @@ public class Room1Controller {
 
   @FXML private Character character;
   @FXML private Rectangle rect1;
+  @FXML private Rectangle rect2;
+  @FXML private Rectangle rect3;
+  @FXML private Rectangle rect4;
+  @FXML private Rectangle rect5;
+  @FXML private Rectangle rect6;
+  @FXML private Rectangle rect7;
+  @FXML private Rectangle rect8;
+  @FXML private Rectangle rect9;
+  @FXML private Rectangle rect10;
+  @FXML private Rectangle rect11;
+  @FXML private Rectangle rect12;
+  @FXML private Rectangle rect13;
   private MainGame parentController;
 
   /** Initializes the room view, it is called when the room loads. */
   public void initialize() {
 
-    ArrayList<Rectangle> obsts = new ArrayList<Rectangle>();
-    obsts.add(0, rect1);
+    ArrayList<Rectangle> obsts =
+        new ArrayList<Rectangle>(
+            Arrays.asList(
+                rect1, rect2, rect3, rect4, rect5, rect6, rect7, rect8, rect9, rect10, rect11,
+                rect12, rect13));
+    // obsts.add(0, rect1);
     // Initialization code goes here
     character.enableMobility(obsts);
     character.setLayoutX(250);
