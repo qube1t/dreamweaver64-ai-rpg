841cf191615fa7ae068cb154efd8cd22ecaa8248
skim411 <skim411@aucklanduni.ac.nz>
2023-09-11 06:13:37 +1200

    created controller for password window

---
+++ src/main/java/nz/ac/auckland/se206/controllers/PasswordController.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class PasswordController {
+    
+    @FXML private PasswordField password;
+
+    
+
+}
