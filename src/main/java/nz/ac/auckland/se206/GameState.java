15b8a2be37b236bedebe707e544a6207e782a16c
skim411 <skim411@aucklanduni.ac.nz>
2023-09-15 03:14:08 +1200

    fixed the bug that charecter does not move after visiting the treasure box using overlay

--- src/main/java/nz/ac/auckland/se206/GameState.java
+++ src/main/java/nz/ac/auckland/se206/GameState.java
@@ -9,7 +9,6 @@ public class GameState {
   /** Indicates whether the key has been found. */
   public static boolean isKeyFound = false;
 
-
   public static boolean isPreviousFlightPlanOpen = false;
   public static boolean isDepBoardOpen = false;
   public static boolean isCorrectRouteFound = false;
@@ -18,4 +17,23 @@ public class GameState {
 
   public static String[] booksInRoom1 = new String[7];
 
+  /** Indicates whether the book has found to trade with pirate */
+  public static boolean isBookFound = true;
+
+  /** Indicates whether the box key has found to open the treasure box */
+  public static boolean isBoxKeyFound = false;
+
+  /** Indicates whether the access key has found to access the computer */
+  public static boolean isAccessKeyFound = false;
+
+  /** Indicates whether the treasure has found */
+  public static boolean isTreasureFound = false;
+
+  /** Indicates whether the wrong treasure box has clicked */
+  public static boolean isWrongTreasurBoxClicked = false;
+
+  /** Indicates whether the radar computer has checked */
+  public static boolean isRadarComputerChecked = false;
+
+  public static int boxLocation;
 }
