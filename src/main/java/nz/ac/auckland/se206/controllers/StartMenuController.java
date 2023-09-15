a8f02d6d7f0bd886d21a0f3bbc43561c9e6119f0
Yuewen Sun <ysun755@aucklanduni.ac.nz>
2023-09-15 12:28:26 +1200

    Fixed bug in defualt difficulty and time limit.

--- src/main/java/nz/ac/auckland/se206/controllers/StartMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartMenuController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.GameState;
 
 public class StartMenuController {
   @FXML private ComboBox<String> difficulty;
@@ -13,7 +14,7 @@ public class StartMenuController {
   @FXML Button startButton;
 
   public void initialize() {
-    difficulty.getItems().addAll("Easy", "Medium", "Hard");
+    difficulty.getItems().addAll("EASY", "MEDIUM", "HARD");
     timeLimit.getItems().addAll("2 minutes", "4 minutes", "6 minutes");
   }
 
@@ -23,8 +24,21 @@ public class StartMenuController {
     String difficulty = this.difficulty.getValue();
     String timeLimit = this.timeLimit.getValue();
 
+    // If the difficulty or time limit is default, set it to easy and 2 minutes respectively
+    if (difficulty == null) {
+      difficulty = "EASY";
+    }
+    if (timeLimit == null) {
+      timeLimit = "2 minutes";
+    }
+
+    GameState.gameMode = new String[] {difficulty, timeLimit};
     System.out.println(
-        "Game started with" + difficulty + " difficulty and " + timeLimit + " time limit");
+        "Game started with"
+            + GameState.gameMode[0]
+            + " difficulty and "
+            + GameState.gameMode[1]
+            + " time limit");
 
     // Transition to the main game view
     try {
