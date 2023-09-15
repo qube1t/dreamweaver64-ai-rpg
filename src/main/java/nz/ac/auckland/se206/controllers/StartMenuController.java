a2dc547d65d20624483fd437bcbfbea32014535b
Yuewen Sun <ysun755@aucklanduni.ac.nz>
2023-09-15 12:13:23 +1200

    Implemented start button and display the difficuly level and time limit the player selected.

--- src/main/java/nz/ac/auckland/se206/controllers/StartMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartMenuController.java
@@ -1,14 +1,27 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
 public class StartMenuController {
   @FXML private ComboBox<String> difficulty;
   @FXML private ComboBox<String> timeLimit;
 
+  @FXML Button startButton;
+
   public void initialize() {
     difficulty.getItems().addAll("Easy", "Medium", "Hard");
     timeLimit.getItems().addAll("2 minutes", "4 minutes", "6 minutes");
+  }
+
+  @FXML
+  public void onClickStartButton() {
+    // Get the difficulty and time limit the user selected
+    String difficulty = this.difficulty.getValue();
+    String timeLimit = this.timeLimit.getValue();
+
+    System.out.println(
+        "Game started with" + difficulty + " difficulty and " + timeLimit + " time limit");
   }
 }
