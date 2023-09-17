e2cdbc41c8f014b04103437abede1d678208015c
Samuel Kurian <samuelrkurian@gmail.com>
2023-09-17 23:37:20 +1200

    privated method for non random book selection

--- src/main/java/nz/ac/auckland/se206/Helper.java
+++ src/main/java/nz/ac/auckland/se206/Helper.java
@@ -1,16 +1,19 @@
 package nz.ac.auckland.se206;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Helper {
-  public static void setBooksInRoom1() {
-    GameState.booksInRoom1 =
-        new String[] {
-          "The Great Gatsby",
-          "The Catcher in the Rye",
-          "The Grapes of Wrath",
-          "To Kill a Mockingbird",
-          "The Color Purple",
-          "Ulysses",
-          "Beloved"
-        };
+  public static List<String> getBetweenChar(String str, String c) {
+    List<String> matchesList = new ArrayList<String>();
+    Pattern pattern = Pattern.compile("\\" + c + "(.*?)\\" + c);
+    Matcher m1 = pattern.matcher(str);
+
+    while (m1.find()) {
+      matchesList.add(m1.group().replace(c, ""));
+    }
+    return matchesList;
   }
 }
