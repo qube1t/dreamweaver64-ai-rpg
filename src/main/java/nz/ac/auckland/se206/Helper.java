1a8440b10ef1741bcfa4d5c103a7409e355c8c95
skim411 <skim411@aucklanduni.ac.nz>
2023-09-20 05:55:41 +1200

    added GPTEngine for sending request and return response

--- src/main/java/nz/ac/auckland/se206/Helper.java
+++ src/main/java/nz/ac/auckland/se206/Helper.java
@@ -1,5 +1,10 @@
 package nz.ac.auckland.se206;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Helper {
   public static void setBooksInRoom1() {
     GameState.booksInRoom1 =
@@ -12,5 +17,21 @@ public class Helper {
           "Ulysses",
           "Beloved"
         };
+  }
+
+  public static List<String> getTextBetweenChar(String str, String c) {
+    List<String> matchesList = new ArrayList<String>();
+    Pattern pattern = Pattern.compile("\\" + c + "(.*?)\\" + c);
+    Matcher m1 = pattern.matcher(str);
+
+    while (m1.find()) {
+      matchesList.add(m1.group().replace(c, ""));
+    }
+    return matchesList;
+  }
+
+  // https://www.baeldung.com/java-generating-random-numbers-in-range
+  public static int getRandomNumber(int min, int max) {
+    return (int) ((Math.random() * (max - min)) + min);
   }
 }
