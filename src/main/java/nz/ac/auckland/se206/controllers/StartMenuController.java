2d1fd9c49a3b414c40f6f4cd8befe04cb907c391
Samuel Kurian <samuelrkurian@gmail.com>
2023-09-20 23:34:08 +1200

    added splash screen with progress bar; refactored code to load during splash

--- src/main/java/nz/ac/auckland/se206/controllers/StartMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartMenuController.java
@@ -1,18 +1,11 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
-import java.util.List;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
-import nz.ac.auckland.se206.GptEngine;
-
-import nz.ac.auckland.se206.Helper;
-import nz.ac.auckland.se206.gpt.GptPromptEngineeringRoom1;
-
 import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
 
 public class StartMenuController {
@@ -27,17 +20,6 @@ public class StartMenuController {
     timeLimit.getItems().addAll("2 minutes", "4 minutes", "6 minutes");
 
     // new GptEngine();
-
-    GameState.eleanorAi.runGpt(GptPromptEngineeringRoom1.gameIntro());
-    GameState.eleanorAi.runGpt(
-        GptPromptEngineeringRoom1.gameInstructions(),
-        s -> {
-          List<String> pirateDialogue = Helper.getTextBetweenChar(s, "#");
-          if (pirateDialogue.size() > 0) {
-            GameState.instructionMsg = pirateDialogue.get(0);
-          }
-          Platform.runLater(() -> startButton.setDisable(false));
-        });
 
   }
 
