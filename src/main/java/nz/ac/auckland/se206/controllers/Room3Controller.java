6d40c6be9dfcb06a89a9d34b38716102e8e7df55
Yuewen Sun <ysun755@aucklanduni.ac.nz>
2023-10-03 17:50:04 +1300

    Moved the mission control map to main room.

--- src/main/java/nz/ac/auckland/se206/controllers/Room3Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room3Controller.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,6 +16,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.Helper;
 import nz.ac.auckland.se206.components.Character;
@@ -84,10 +91,39 @@ public class Room3Controller {
   private ImageView paperImage;
   @FXML
   private Pane clickPane;
+  @FXML
+  private Text label1;
+  @FXML
+  private Text label2;
+  @FXML
+  private Text label3;
+  @FXML
+  private Text label4;
+  @FXML
+  private Text label5;
+  @FXML
+  private Circle point1;
+  @FXML
+  private Circle point2;
+  @FXML
+  private Circle point3;
+  @FXML
+  private Circle point4;
+  @FXML
+  private Circle point5;
+
+  private Circle[] cityPoints;
+  private Text[] cityLabels;
+  private Timeline radarAnimation;
+  private int currentCity;
+  private Circle currentCityPoint;
+  private Text currentCityLabel;
 
   private ArrayList<Rectangle> obstacles;
 
   public void initialize() throws ApiProxyException {
+
+    initilizeGpsMap();
 
     // Generate seven random city destnations and randomly choose one of them
     // for the puzzle game if it is not set
@@ -340,4 +376,75 @@ public class Room3Controller {
     MainGameController.addOverlay("room1", true);
     GameState.eleanorAi.runGpt("User update: User has moved from ATC to his childhood home room.");
   }
+
+  private void initilizeGpsMap() {
+    // Initialize the radar points and radarObjects to a list.
+    this.cityPoints = new Circle[] { point1, point2, point3, point4, point5 };
+    this.cityLabels = new Text[] { label1, label2, label3, label4, label5 };
+
+    GameState.currentCities = cityLabels;
+
+    // initalize current city if it is not set
+    if (GameState.currentCityIndex == -1) {
+
+      // generate a number between 1 and 5
+      int randomCity = (int) (Math.random() * 5 + 1);
+      GameState.currentCityIndex = randomCity;
+      this.currentCity = randomCity;
+    }
+
+    this.currentCity = GameState.currentCityIndex;
+
+    this.currentCityPoint = cityPoints[currentCity - 1];
+    this.currentCityLabel = cityLabels[currentCity - 1];
+
+    // Set invisible at the start
+    label1.setVisible(false);
+    label2.setVisible(false);
+    label3.setVisible(false);
+    label4.setVisible(false);
+    label5.setVisible(false);
+    point1.setVisible(false);
+    point2.setVisible(false);
+    point3.setVisible(false);
+    point4.setVisible(false);
+    point5.setVisible(false);
+
+    currentCityLabel.setVisible(true);
+    // Initialize the radarAnimation timeline
+    this.radarAnimation = new Timeline(
+        new KeyFrame(Duration.seconds(0), event -> fadeInRadarPoints(currentCityPoint)),
+        new KeyFrame(Duration.seconds(1), event -> fadeOutRadarPoints(currentCityPoint)));
+    radarAnimation.setCycleCount(Timeline.INDEFINITE);
+    radarAnimation.setOnFinished(
+        event -> {
+          // Restart the animation when it completes
+          radarAnimation.play();
+        });
+
+    // Start the animation
+    radarAnimation.play();
+
+    System.out.println("Current city is " + GameState.currentCities[currentCity - 1].getText());
+  }
+
+  protected void fadeInRadarPoints(Circle city) {
+
+    city.setVisible(true);
+    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1), city);
+    fadeTransition.setFromValue(0.0);
+    fadeTransition.setToValue(1.0);
+    fadeTransition.setInterpolator(Interpolator.LINEAR); // Use linear interpolation
+    fadeTransition.play();
+  }
+
+  protected void fadeOutRadarPoints(Circle city) {
+
+    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1.5), city);
+    fadeTransition.setFromValue(1.0);
+    fadeTransition.setToValue(0.0);
+    fadeTransition.setInterpolator(Interpolator.LINEAR); // Use linear interpolation
+    fadeTransition.play();
+  }
+
 }
