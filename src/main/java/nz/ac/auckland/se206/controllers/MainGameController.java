59cf8e7661998b79e3bce9d317dc84137c3ae561
Samuel Kurian <samuelrkurian@gmail.com>
2023-10-07 00:39:51 +1300

    fixed merge bugs with visibility

--- src/main/java/nz/ac/auckland/se206/controllers/MainGameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MainGameController.java
@@ -168,8 +168,8 @@ public class MainGameController {
     // adding both to game pane
     initialisedGamePane
         .getChildren()
-        .add(initialisedGamePane.getChildren().size() - 3, backgroundBlur);
-    initialisedGamePane.getChildren().add(initialisedGamePane.getChildren().size() - 3, room1);
+        .add(initialisedGamePane.getChildren().size() - 4, backgroundBlur);
+    initialisedGamePane.getChildren().add(initialisedGamePane.getChildren().size() - 4, room1);
   }
 
   public static void removeOverlay(boolean alsoRooms) {
@@ -180,13 +180,13 @@ public class MainGameController {
       // removing rooms as well
       sub = 2;
     }
-    if (initialisedGamePane.getChildren().size() > 5 - sub) {
+    if (initialisedGamePane.getChildren().size() > 6 - sub) {
       initialisedGamePane
           .getChildren()
-          .remove(initialisedGamePane.getChildren().size() - 1 - 3);
+          .remove(initialisedGamePane.getChildren().size() - 1 - 4);
       initialisedGamePane
           .getChildren()
-          .remove(initialisedGamePane.getChildren().size() - 1 - 3);
+          .remove(initialisedGamePane.getChildren().size() - 1 - 4);
       initialisedGamePane.requestFocus();
     }
   }
@@ -459,9 +459,9 @@ public class MainGameController {
           } else if (currentTime == 10) {
             // 10 seconds left
             GameState.tenSecondsLeft = true;
-            //Room1Controller.setEndImg();
+            // Room1Controller.setEndImg();
             Room2Controller.setEndImg();
-            //Room3Controller.setEndImg();
+            // Room3Controller.setEndImg();
           }
           int time = currentTime;
           int minutes = time / 60;
