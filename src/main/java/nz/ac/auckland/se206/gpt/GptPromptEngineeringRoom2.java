059d1548172c8f43cc027ea17178c276845d6f48
skim411 <skim411@aucklanduni.ac.nz>
2023-10-17 17:41:10 +1300

    fixed the gpt prompt for room2

--- src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineeringRoom2.java
+++ src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineeringRoom2.java
@@ -64,7 +64,7 @@ public class GptPromptEngineeringRoom2 {
    * @return the encrypted message
    */
   public static String generateFinalEncrypted() {
-    return "This is the loading stage. Return meaningless encryption of 1 sentence, and only"
+    return "This is the loading stage. Return meaningless encryption of 60 characters, and only"
         + " return the encryption with the character + before and after. ";
   }
 }
